6571ca207f971e230b84105680713a2e92f800f4
MJC-Studios <michaelcope422@gmail.com>
2026-07-11 22:02:51 -0400

    Webpage background added

---
+++ script.js
@@ -0,0 +1,28 @@
+const images = [
+    "images/backgrounds/farm background.png",
+    "images/backgrounds/sakura background.png",
+    "images/backgrounds/ironhead background.png",
+    "images/backgrounds/Abandoned outpost background.png",
+    "images/backgrounds/silverhollow background.png",
+    "images/backgrounds/zeppelin background.png",
+    "images/backgrounds/convension background.png"
+];
+
+let current = 0;
+
+const hero =
+document.getElementById("hero");
+
+function changeBackground(){
+    hero.style.backgroundImage =
+    `linear-gradient(rgba(0, 0, 0, 0.55), rgba(0, 0, 0, 0.55)), url(${images[current]})`;
+
+    current++;
+    if(current >= images.length){
+        current = 0;
+    }
+}
+
+changeBackground();
+
+setInterval(changeBackground, 5000);
